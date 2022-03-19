11a58212017c9c3ccebfc19d58ce1a258446a326
Abhibhav Om Tyagi <tyagi.abhibhav@gmail.com>
2022-03-19 13:13:52 +0530

    #Commit-1.02

--- app/src/main/java/com/example/writeout/MainActivity.java
+++ app/src/main/java/com/example/writeout/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.writeout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
