ed7f0bdce70d48668ff526e840e097241c500189
Abhibhav Om Tyagi <tyagi.abhibhav@gmail.com>
2022-03-20 14:22:01 +0530

    #Commit-1.03

--- app/src/main/java/com/example/writeout/MainActivity.java
+++ app/src/main/java/com/example/writeout/MainActivity.java
@@ -9,12 +9,31 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText Email1,Email2,Uname1,Uname2,Pass1,Pass2,RePass,OTP;
+    private TextView Login,FPass,Signup;
+    private Button B1,B2,B3,B4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Email1=findViewById(R.id.e1);
+        Email2=findViewById(R.id.e2);
+        Uname1=findViewById(R.id.u1);
+        Uname2=findViewById(R.id.u2);
+        Pass1=findViewById(R.id.p0);
+        RePass=findViewById(R.id.p1);
+        Pass2=findViewById(R.id.p2);
+        OTP=findViewById(R.id.otp);
 
+        Login=findViewById(R.id.LIn);
+        FPass=findViewById(R.id.p3);
+        Signup=findViewById(R.id.SUp);
 
+        B1=findViewById(R.id.b1);
+        B2=findViewById(R.id.b2);
+        B3=findViewById(R.id.b3);
+        B4=findViewById(R.id.b4);
     }
 }
